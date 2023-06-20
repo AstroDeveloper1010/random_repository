d26769892480f33cdbf8e7b9d7031adc3096fe40
Priyanshu Jha <priyanshujha131@gmail.com>
2023-06-20 10:08:18 +0530

    updated

---
+++ js/goa3d.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000, // Adjust the delay (in milliseconds) between slides
+    disableOnInteraction: false // Enable/disable autoplay on user interaction
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
